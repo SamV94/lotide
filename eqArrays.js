61bac58c94cf24a6db6375c233d1d7627e0f0160
Sam Vardanian <sam.vardanian@hotmail.ca>
2021-04-29 20:06:50 -0400

    Array Equals Assertion

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+function assertEqual(actual, expected) {
+  if(actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}!`)
+  } else (console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}!`))
+}
+
+
+function eqArrays(firstArr, secondArr) {
+  for(let i = 0; i < firstArr.length; i++){
+    if(firstArr[i] !== secondArr[i]) {
+      return false } 
+  };
+  return true;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+
+
+
+
+
